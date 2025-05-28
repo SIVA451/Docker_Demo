e3bc68062d7c26bd13642e134c9988d687a3b067
sivasankar.nagireddy <13656990+SIVA451@users.noreply.github.com>
2025-05-28 07:40:26 +0530

    Update app.py

--- demo/app.py
+++ demo/app.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Docker on Amazon Linux 2!\n"
+    return "Hello from Docker Demo!\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
